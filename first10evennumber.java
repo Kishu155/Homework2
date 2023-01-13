d95e85e326a2c51deef7caa34a2ba29b6640df8c
Krishna kanani <krishnakanani1995@gmail.com>
2023-01-13 23:06:12 +0000

    first commit

---
+++ first10evennumber.java
@@ -0,0 +1,8 @@
+package Homework2;
+
+public class first10evennumber {
+    public static void main(String[] args) {
+        for ( int i=1;i<=10;i++)
+            System.out.println(i*2);
+    }
+}
